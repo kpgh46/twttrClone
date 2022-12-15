eee7575cd9424ff5ce999c60c7ef4122fcebe5ce
Kevin McPeak <kevmc46@gmail.com>
2022-12-15 17:55:10 -0500

    created TweetCard component. Refactored

--- frontend/src/components/TweetCard.js
+++ frontend/src/components/TweetCard.js
@@ -3,21 +3,64 @@ import Card from "react-bootstrap/Card";
 import { useContext, useEffect } from "react";
 import { authContext } from "../context/authContext";
 import { TweetContext } from "../context/tweetContext";
-import formatDistanceToNow from "date-fns/formatDistanceToNow";
+// import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
-const TweetCard = (props) => {
+const TweetCard = () => {
+	const { loggedInUser } = useContext(authContext);
+	const { tweets, setTweets } = useContext(TweetContext);
+
+	useEffect(() => {
+		const fetchTweets = async () => {
+			let response = await fetch("api/tweets", {
+				headers: {
+					Authorization: `Bearer ${loggedInUser.token}`,
+				},
+			});
+			let json = await response.json();
+
+			if (response.ok) {
+				setTweets(json);
+			}
+		};
+
+		if (loggedInUser) {
+			fetchTweets();
+		}
+	}, [loggedInUser]);
+
+	const addLike = async (_id) => {
+		let response = await fetch("api/addlike", {
+			method: "PATCH",
+			body: JSON.stringify({ _id }),
+			headers: {
+				Authorization: `Bearer ${loggedInUser.token}`,
+				"Content-Type": "application/json",
+			},
+		});
+		const json = await response.json();
+
+		if (response.ok) {
+			console.log("this worked from addLike", json);
+
+			// setUsers(json);
+		}
+	};
 	return (
 		<div>
-			{props.tweets.map((tweet) => (
-				<Card>
-					<p key={tweet._id}>
-						{tweet.author} - {tweet.caption} {tweet.likes}
-						{formatDistanceToNow(new Date(tweet.createdAt), {
-							addSuffix: true,
-						})}
-					</p>
-				</Card>
-			))}
+			{tweets &&
+				tweets.map((tweet) => (
+					<Card>
+						<div key={tweet._id}>
+							<div>Caption: {tweet.caption}</div>
+							<div>UserName:{tweet.author.username}</div>
+							<div>Likes: {tweet.likes}</div>
+							<button onClick={() => addLike(tweet._id)}>
+								UP
+							</button>
+							<div>ReShare: {tweet.retweets}</div>
+						</div>
+					</Card>
+				))}
 		</div>
 	);
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,57 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CreateTweet from "../components/CreateTweet";
 import TweetCard from "../components/TweetCard";
 import Navbar from "../components/Navbar";
 import FollowUsers from "../components/FollowUsers";
-import { useContext, useState } from "react";
 
-import { authContext } from "../context/authContext";
-import { TweetContext } from "../context/tweetContext";
-
-// import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 let Home = () => {
-	const { loggedInUser } = useContext(authContext);
-	const { tweets, setTweets } = useContext(TweetContext);
-
-	useEffect(() => {
-		const fetchTweets = async () => {
-			let response = await fetch("api/tweets", {
-				headers: {
-					Authorization: `Bearer ${loggedInUser.token}`,
-				},
-			});
-			let json = await response.json();
-
-			if (response.ok) {
-				setTweets(json);
-			}
-		};
-
-		if (loggedInUser) {
-			fetchTweets();
-		}
-	}, [loggedInUser]);
-
-	const addLike = async (_id) => {
-		let response = await fetch("api/addlike", {
-			method: "PATCH",
-			body: JSON.stringify({ _id }),
-			headers: {
-				Authorization: `Bearer ${loggedInUser.token}`,
-				"Content-Type": "application/json",
-			},
-		});
-		const json = await response.json();
-
-		if (response.ok) {
-			console.log("this worked from addLike", json);
-
-			// setUsers(json);
-		}
-	};
-
 	return (
 		<div className="container border">
 			<Navbar />
@@ -59,19 +14,7 @@ let Home = () => {
 				<div className="col-3 border">This is the far left row</div>
 				<div className="col-6">
 					<CreateTweet />
-					{tweets &&
-						tweets.map((tweet) => (
-							<div key={tweet._id}>
-								<div>Caption: {tweet.caption}</div>
-								<div>UserName:{tweet.author.username}</div>
-								<div>Likes: {tweet.likes}</div>
-								<button onClick={() => addLike(tweet._id)}>
-									UP
-								</button>
-								<div>ReShare: {tweet.retweets}</div>
-								<div>---BREAK---</div>
-							</div>
-						))}
+					<TweetCard />
 				</div>
 				<div className="col border">
 					<h1>USER list</h1>
